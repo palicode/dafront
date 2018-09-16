c89b542b70cfba2fc77f1d77772b8838647fb227
Etor <etor@Onas-MacBook-Pro.local>
2018-09-16 21:06:27 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../styles/App.css';
+
+const App = () => (
+  <div>
+    <h1>My React live!</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './assets/index.html',
+    }),
+  ],
+};
